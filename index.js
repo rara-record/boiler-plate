d5a60f90111798433673e149785760c099b933ee
BORA <a52447879@gmail.com>
2021-11-17 03:08:51 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended : true}));
+
+// application/json
+app.use(express.json());
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://bora:ssd10237879@bolierplate.oewiw.mongodb.net/bora?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser : true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/', (req, res) => res.send('Hello World! 코코 바보'))
+
+
+app.post('/register', (req, res) => {
+    
+  // 회원 가입 할떄 필요한 정보들을 cclient에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+        success: true
+    })
+  })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
